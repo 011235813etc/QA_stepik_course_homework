084aeba6c829f174444f15abfb10bbdc2d1d38dd
Ilgiz <ii01123581321@gmail.com>
2021-02-13 00:04:09 +0500

    Added example code in main_page.py and locatiors.py from lesson 2 step 7

---
+++ chapter_4/pages/locators.py
@@ -0,0 +1,10 @@
+"""
+https://stepik.org/lesson/238819/step/7?unit=211271
+example from lesson 2 step 7
+"""
+
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
