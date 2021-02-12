7753b278c28e4937029ed1bb2596fa159dc8ee90
Ilgiz <ii01123581321@gmail.com>
2021-02-12 22:59:32 +0500

    example from lesson 1 step 7

--- chapter_4/test_main_page.py
+++ chapter_4/test_main_page.py
@@ -1,5 +1,15 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+"""
+https://stepik.org/lesson/199980/step/7?unit=174035
+example from lesson 1 step 7
+"""
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+def go_to_login_page(browser):
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
+
+
+def test_guest_can_go_to_login_page(browser):
+    browser.get(link)
+    go_to_login_page(browser)
