1323d6bb84d1b87d9d34b68918b38b6191e38d86
Ilgiz <ii01123581321@gmail.com>
2021-02-12 23:49:37 +0500

    Added example code in test_main_page.py and main_page.py from lesson 2 step 5, raise an exception

--- chapter_4/pages/main_page.py
+++ chapter_4/pages/main_page.py
@@ -1,6 +1,8 @@
 """
-https://stepik.org/lesson/238819/step/3?unit=211271
-example from lesson 2 step 3
+https://stepik.org/lesson/238819/step/5?unit=211271
+example from lesson 2 step 5
+
+В качестве ответа на данное задание напишите название исключения, которое вы получили в результате запуска теста.
 """
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
@@ -10,3 +12,6 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
         login_link.click()
+
+    def should_be_login_link(self):
+        self.browser.find_element(By.CSS_SELECTOR, "#login_link_invalid")
--- chapter_4/test_main_page.py
+++ chapter_4/test_main_page.py
@@ -1,6 +1,6 @@
 """
-https://stepik.org/lesson/238819/step/4?unit=211271
-example from lesson 2 step 4
+https://stepik.org/lesson/238819/step/5?unit=211271
+example from lesson 2 step 5
 """
 from .pages.main_page import MainPage
 
@@ -10,3 +10,10 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
