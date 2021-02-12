41d35a5b34bc0934911486fc31712b2e3544f9b5
Ilgiz <ii01123581321@gmail.com>
2021-02-12 23:42:53 +0500

    Added example code in test_main_page.py from lesson 2 step 4

--- chapter_4/test_main_page.py
+++ chapter_4/test_main_page.py
@@ -1,15 +1,12 @@
 """
-https://stepik.org/lesson/199980/step/7?unit=174035
-example from lesson 1 step 7
+https://stepik.org/lesson/238819/step/4?unit=211271
+example from lesson 2 step 4
 """
-link = "http://selenium1py.pythonanywhere.com/"
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
-    browser.get(link)
-    go_to_login_page(browser)
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
