f3e0fb3f78dbca26698c10b03f8d4bf64ea5e020
Ilgiz <ii01123581321@gmail.com>
2021-02-13 14:28:49 +0500

    Added example from lesson 2 step 9, homework LoginPage was changed

--- chapter_4/pages/locators.py
+++ chapter_4/pages/locators.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 
 
 class MainPageLocators():
-    LOGIN_LINK = (By.ID, "#login_link")
+    LOGIN_LINK = (By.ID, "login_link")
 
 
 class LoginPageLocators():
--- chapter_4/pages/main_page.py
+++ chapter_4/pages/main_page.py
@@ -1,6 +1,6 @@
 """
-https://stepik.org/lesson/238819/step/7?unit=211271
-example from lesson 2 step 7
+https://stepik.org/lesson/238819/step/9?unit=211271
+example from lesson 2 step 9
 """
 
 from .base_page import BasePage
@@ -11,6 +11,7 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
