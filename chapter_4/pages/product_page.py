3a2b012b1ee817af1048fe8615ebccb9ef7b7a48
Ilgiz <ii01123581321@gmail.com>
2021-02-13 17:54:03 +0500

    homework from lesson 3 step 3, Independent of data

--- chapter_4/pages/product_page.py
+++ chapter_4/pages/product_page.py
@@ -1,3 +1,4 @@
+from selenium.common.exceptions import NoSuchElementException
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
@@ -10,23 +11,26 @@ class ProductPage(BasePage):
         self.should_be_same_product_price()
 
     def should_be_added_by_button(self):
-        # ищем кнопку
-        button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET)
-        # добавляем в корзину
-        button.click()
-        self.solve_quiz_and_get_code()
-        assert button is not None, "Not found button 'Add to basket'"
+        try:
+            # ищем кнопку и добавляем в корзину
+            button = self.browser.find_element(*ProductPageLocators.ADD_TO_BASKET).click()
+            self.solve_quiz_and_get_code()
+        except NoSuchElementException:
+            print("Not found button 'Add to basket'")
+
+    def _get_description(self, locator):
+        return self.browser.find_element(*locator).text
 
     def should_be_same_product_name(self):
         # получаем название продукта в описании товара
-        name_offered = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_OFFERED).text
+        name_offered = self._get_description(ProductPageLocators.PRODUCT_NAME_OFFERED)
         # получаем название товара в корзине
-        name_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_BASKET).text
+        name_in_basket = self._get_description(ProductPageLocators.PRODUCT_NAME_IN_BASKET)
         assert name_offered == name_in_basket, "Offered name and name in basket is not equal"
 
     def should_be_same_product_price(self):
         # получаем предлагаемую цену товара
-        price_offered = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_OFFERED).text
+        price_offered = self._get_description(ProductPageLocators.PRODUCT_PRICE_OFFERED)
         # получаем цену в корзине
-        price_in_basket = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_BASKET).text
+        price_in_basket = self._get_description(ProductPageLocators.PRODUCT_PRICE_IN_BASKET)
         assert price_offered == price_in_basket, "Offered price and price in basket is not equal"
