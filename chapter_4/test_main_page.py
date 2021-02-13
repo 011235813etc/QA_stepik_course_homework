6913cf2ac65e3c443110a3c4751174ade77dac3c
Ilgiz <ii01123581321@gmail.com>
2021-02-13 13:54:35 +0500

    homework from lesson 2 step 8, LoginPage realization

--- chapter_4/test_main_page.py
+++ chapter_4/test_main_page.py
@@ -3,17 +3,24 @@ https://stepik.org/lesson/238819/step/5?unit=211271
 example from lesson 2 step 5
 """
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
-def test_guest_can_go_to_login_page(browser):
+# def test_guest_can_go_to_login_page(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+#
+#
+# def test_guest_should_see_login_link(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.should_be_login_link()
+
+def test_guest_should_see_1(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
+    page = LoginPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.should_be_login_page()
