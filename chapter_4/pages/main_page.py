702e5167ff8861d57b067c08c799a2581789f08e
Ilgiz <ii01123581321@gmail.com>
2021-02-13 15:01:40 +0500

    Added example from lesson 2 step 10

--- chapter_4/pages/main_page.py
+++ chapter_4/pages/main_page.py
@@ -1,6 +1,6 @@
 """
-https://stepik.org/lesson/238819/step/9?unit=211271
-example from lesson 2 step 9
+https://stepik.org/lesson/238819/step/10?unit=211271
+example from lesson 2 step 10
 """
 
 from .base_page import BasePage
@@ -11,6 +11,10 @@ class MainPage(BasePage):
     def go_to_login_page(self):
         login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
         login_link.click()
+        # если будет добавлен alert в страницы
+        # alert = self.browser.switch_to.alert
+        # alert.accept()
+
         # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
